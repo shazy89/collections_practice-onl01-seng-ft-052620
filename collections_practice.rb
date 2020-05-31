c63096571d876d41c7f6a99d215f65ad86d48afd
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-31 15:45:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(array)
+    array.sort
+end
 
+
+def sort_array_desc(my_array)
+  my_array.sort do |a , b | 
+  b <=> a 
+ end
+
+end
+
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+   array.sort do |a , b|
+     a[1] <=> b[2]
+   end
+ end
+ 
+ def reverse_array(array)
+   array.reverse
+ end
+ 
+ def kesha_maker(array)
+new_array = []
+   array.each do |word| 
+    new_word = word.split("")
+    new_word[2] = "$"
+new_array.push new_word.join
+   end
+   new_array
+     
+ end
+ 
+ def find_a(array)
+   array.find do |word| word.include?("a")
+ end
+ end
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
